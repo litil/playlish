243a7d6b6286232d6a79636ce81387876092db01
Guillaume Lambert <guillaume.p.lambert@gmail.com>
2020-05-22 21:50:35 +0200

    impr(UI): playlist details more responsive

--- src/components/molecules/TrackRow/index.tsx
+++ src/components/molecules/TrackRow/index.tsx
@@ -1,5 +1,6 @@
-import { formatDistance } from 'date-fns';
+import { differenceInCalendarDays, formatDistance } from 'date-fns';
 import React, { FunctionComponent } from 'react';
+import { FaClock, FaFire } from 'react-icons/fa';
 import { getValidDate } from '../../../helpers';
 import './styles.css';
 
@@ -19,14 +20,21 @@ export const TrackRow: FunctionComponent<ITrackRowProps> = ({ header, track }) =
   const dateNow = new Date();
   const dateValue =
     track.added_at === 'Added' ? 'Added' : formatDistance(getValidDate(track.added_at), dateNow);
+  const dateMessage =
+    track.added_at !== 'Added' &&
+    differenceInCalendarDays(new Date(), getValidDate(track.added_at)) > 1
+      ? ` added ${dateValue} ago`
+      : `added ${dateValue}`;
+
+  console.log(differenceInCalendarDays(new Date(), getValidDate(track.added_at)));
 
   return (
-    <div className="flex flex-row justify-between border-b border-customBlue-300 w-full py-4 px-2">
+    <div className="flex flex-col lg:flex-row justify-between border-b border-customBlue-300 w-full py-4 px-2">
       <div className="flex flex-col items-start">
         <span className="text-blue-100 text-left">{trackDetail.name}</span>
         <div className="flex flex-row items-start">
           <span className="text-customBlue-300 italic text-sm">by&nbsp;</span>
-          <span className="text-customBlue-300 text-sm text-left">{`${artist} •`}</span>
+          <span className="text-customBlue-300 text-sm text-left">{`${artist}`}</span>
         </div>
         <div className="flex flex-row items-start">
           <span className="text-customBlue-300 italic text-sm">from&nbsp;</span>
@@ -34,9 +42,15 @@ export const TrackRow: FunctionComponent<ITrackRowProps> = ({ header, track }) =
         </div>
       </div>
 
-      <div className="flex flex-col items-end">
-        <span className="text-customBlue-300 text-sm text-right">{`added ${dateValue}`}</span>
-        <span className="text-customBlue-300 text-sm text-right">{`popularity: ${trackDetail.popularity}`}</span>
+      <div className="flex flex-col items-start lg:items-end text-sm text-customBlue-300 pt-2 lg:pt-0">
+        <div className="flex flex-row items-center ">
+          <FaClock />
+          <span className="ml-2">{dateMessage}</span>
+        </div>
+        <div className="flex flex-row items-center">
+          <FaFire />
+          <span className="ml-2">{`popularity: ${trackDetail.popularity}`}</span>
+        </div>
       </div>
     </div>
   );
